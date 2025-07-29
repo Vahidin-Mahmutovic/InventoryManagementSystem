342198a1fae04ef168da28bd213e13f2733662ec
Vahidin Mahmutović <mahmutovic.vahidin@hotmail.com>
2025-07-29 14:20:40 +0200

    Add Employee Product controller and view

---
+++ IMS/Areas/Employee/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using IMS.DataAccess.Repository.IRepository;
+using IMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMS.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,Unit").ToList();
+
+            return View(productList);
+        }
+    }
+}
