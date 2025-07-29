5988fd6dc0f50853b3149cbb2737d1975bbe6b3f
Vahidin Mahmutović <mahmutovic.vahidin@hotmail.com>
2025-07-29 14:04:43 +0200

    Add Employee StockLevel controller and view

---
+++ IMS/Areas/Employee/Controllers/StockLevelController.cs
@@ -0,0 +1,22 @@
+﻿using IMS.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMS.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class StockLevelController : Controller
+    {
+        private readonly IStockLevelService _stockLevelService;
+
+        public StockLevelController(IStockLevelService stockLevelService)
+        {
+            _stockLevelService = stockLevelService;
+        }
+
+        public IActionResult Index()
+        {
+            var stockLevels = _stockLevelService.GetAll();
+            return View(stockLevels);
+        }
+    }
+}
